824d9ceb9e2d5b1c866aacc3702a50aa3163f47c
Satishkumaryadav2001 <sk3407392@gmail.com>
2024-01-03 08:19:10 +0530

    context api discusion

---
+++ Learn_React/07_react_router/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header/Header"
+import Footer from "./components/footer/Footer";
+import { Outlet } from "react-router-dom";
+function Layout(){
+          <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+          </>
+}
+
+export default Layout
---
+++ Learn_React/08_miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+     <h1>React With Chai Share is important</h1>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
